e0deefe7b7bbf79a321000cc086a01e5513f19f2
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2022-09-17 23:15:57 +0530

    Add files via upload

---
+++ 17.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int a;
+
+    private:
+        int b;
+
+    protected:
+        int c;
+
+    public:
+        Base()
+        {
+            a = 10;
+            b = 20;
+            c = 30;
+        }
+
+        void fun();
+};
+
+class Derived : private Base
+{   
+    public:
+        void display()
+        {
+            cout<<a<<endl;
+            //cout<<b;
+            cout<<c;
+        }
+};
+
+void Base :: fun()
+{
+    cout<<"Base fun\n";
+}
+
+/*
+class Derived2 : public Derived
+{
+    public:
+        void display()
+        {
+            cout<<a<<endl;
+            //cout<<b;
+            cout<<c;
+        }
+};*/
+
+int main()
+{
+    Derived dobj;
+
+    dobj.display();
+    //dobj.fun();
+
+    return 0;
+}
+
